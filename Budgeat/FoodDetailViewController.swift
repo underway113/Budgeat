a39354bda5c0e84ee8699228286044cd5ee055c3
Jeremy Adam <underway113@gmail.com>
2019-04-28 02:35:31 +0700

    add dismissCustom + Gesture swipe right to dismiss at FoodDetail Screen

--- Budgeat/FoodDetailViewController.swift
+++ Budgeat/FoodDetailViewController.swift
@@ -9,9 +9,19 @@
 import UIKit
 
 class FoodDetailViewController: UIViewController {
-
+    
     @IBAction func returnButton(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        dismissCustom()
+    }
+    
+    func dismissCustom() {
+        let transition: CATransition = CATransition()
+        transition.duration = 0.2
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = CATransitionType.reveal
+        transition.subtype = CATransitionSubtype.fromLeft
+        self.view.window!.layer.add(transition, forKey: nil)
+        self.dismiss(animated: false, completion: nil)
     }
     
     @IBOutlet weak var categoryLabel: UILabel!
@@ -27,49 +37,54 @@ class FoodDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        print("Passed Data = " + passedData?.name)
+        
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        
+        
+        //        print("Passed Data = " + passedData?.name)
         categoryLabel.text = passedData?.name
         foodDetailImage.image = passedData?.image
         priceLabel.text = "\(passedData?.price ?? 0)"
         
-//        categoryLabel.text = selectedName
-       // priceLabel.text = "Rp." + receivedPrice!
-//        foodDetailImage.image = selectedPic
+        //        categoryLabel.text = selectedName
+        // priceLabel.text = "Rp." + receivedPrice!
+        //        foodDetailImage.image = selectedPic
         
-//        if Int(receivedPrice!) == nil {
-//            intReceivedPrice = 0
-//        }
-//        else {
-//            intReceivedPrice = Int(receivedPrice!)
-//        }
-//
-//        switch intReceivedPrice {
-//        case _ where intReceivedPrice! > 0 && intReceivedPrice! <= 20_000:
-//            dollarLabel.text = "$"
-//            dollarLabel.textColor = UIColor(red: 0.15, green: 0.9, blue: 0.2, alpha: 1)
-//        case _ where intReceivedPrice! > 20_000 && intReceivedPrice! <= 40_000:
-//            dollarLabel.text = "$$"
-//            dollarLabel.textColor = UIColor(red: 0.6, green: 0.4, blue: 0.4, alpha: 1)
-//        case _ where intReceivedPrice! > 40_000:
-//            dollarLabel.text = "$$$"
-//            dollarLabel.textColor = UIColor(red: 1, green: 0.1, blue: 0.05, alpha: 1)
-//        default:
-//            dollarLabel.text = "!"
-//            dollarLabel.textColor = UIColor(red: 20, green: 20, blue: 20, alpha: 0.1)
-//        }
-
+        //        if Int(receivedPrice!) == nil {
+        //            intReceivedPrice = 0
+        //        }
+        //        else {
+        //            intReceivedPrice = Int(receivedPrice!)
+        //        }
+        //
+        //        switch intReceivedPrice {
+        //        case _ where intReceivedPrice! > 0 && intReceivedPrice! <= 20_000:
+        //            dollarLabel.text = "$"
+        //            dollarLabel.textColor = UIColor(red: 0.15, green: 0.9, blue: 0.2, alpha: 1)
+        //        case _ where intReceivedPrice! > 20_000 && intReceivedPrice! <= 40_000:
+        //            dollarLabel.text = "$$"
+        //            dollarLabel.textColor = UIColor(red: 0.6, green: 0.4, blue: 0.4, alpha: 1)
+        //        case _ where intReceivedPrice! > 40_000:
+        //            dollarLabel.text = "$$$"
+        //            dollarLabel.textColor = UIColor(red: 1, green: 0.1, blue: 0.05, alpha: 1)
+        //        default:
+        //            dollarLabel.text = "!"
+        //            dollarLabel.textColor = UIColor(red: 20, green: 20, blue: 20, alpha: 0.1)
+        //        }
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func handleGesture(gesture: UISwipeGestureRecognizer) -> Void {
+        dismissCustom()
     }
-    */
-
+    //        else if gesture.direction == UISwipeGestureRecognizer.Direction.left {
+    //            print("Swipe Left")
+    //        }
+    
+    
 }
