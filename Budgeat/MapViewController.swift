68cf284bae296ba2768914114a1f289e9c263e71
Evan Christian <evanchristian98@gmail.com>
2019-04-29 16:16:55 +0700

    annotation detail

--- Budgeat/MapViewController.swift
+++ Budgeat/MapViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var detailMapView: UIView!
+    @IBOutlet weak var detailImage: UIImageView!
+    @IBOutlet weak var detailRestaurant: UILabel!
+    @IBOutlet weak var detailPriceRange: UITextView!
     
     var locationManager = CLLocationManager()
     var userLocation:CLLocation = CLLocation(latitude: -6.222979, longitude: 106.652451)
@@ -32,6 +36,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadLocationData()
+        detailMapView.isHidden = true
         self.locationManager.requestWhenInUseAuthorization()
         if CLLocationManager.locationServicesEnabled() {
             locationManager.delegate = self //as? CLLocationManagerDelegate
@@ -39,9 +44,14 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
         }
         
+        // mapView(mapView, annotationView: LocationsMarkerView, calloutAccessoryControlTapped: <#T##UIControl#>)
+        
+        detailMapView.alpha = 1
+        
         // Initialize map
         mapView.register(LocationsMarkerView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         mapView.showsUserLocation = true
+        mapView.delegate = self
         
         mapView.userTrackingMode = .follow
         
@@ -58,11 +68,37 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         let locValue:CLLocationCoordinate2D = manager.location!.coordinate
         userLocation = CLLocation(latitude: locValue.latitude, longitude: locValue.longitude)
         print("locations = \(locValue.latitude) \(locValue.longitude)")
+        
     }
     
     func centerMapOnLocation(location: CLLocation) {
         let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    //Detail click(callout)
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        print("asd")
+        if control == view.rightCalloutAccessoryView {
+            print("button tapped")
+        }
+        print("tes")
+        detailMapView.isHidden = false
+        
+        
+        let locationName = (view.annotation?.title as? String)
+        for location in locationsMark {
+            if locationName == location.title {
+                detailImage.image = location.image
+                detailRestaurant.text = location.locationName
+                detailPriceRange.text = location.locationDesc
+            }
+        }
+    }
+    
+    //Deselect marker
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        detailMapView.isHidden = true
     }
     
     
